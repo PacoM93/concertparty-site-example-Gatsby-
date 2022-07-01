617a49456db42030493e70a0f7a5b72743b631b1
Francisco Montes <paco@sharpydev.com>
2022-07-01 16:54:05 -0600

    Add mainnavbar scss

---
+++ src/components/Navbar/MainNavbar.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import { Navbar, Nav, Container } from "react-bootstrap"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Link } from "gatsby"
+
+import "./mainNavbar.scss"
+
+const MainNavbar = () => {
+  return (
+    <Navbar bg="light" expand="lg" className="main-navbar">
+      <Container>
+        <Navbar.Brand href="#home">
+          <span className="logo-concert">Concert</span>
+          <span className="logo-party">Party</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Link to="/concerts" className="nav-link">
+              Featured concerts
+            </Link>
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default MainNavbar
