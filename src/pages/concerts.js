73e267b867b120f3df58ec126bf83137de5caa16
Francisco Montes <paco@sharpydev.com>
2022-07-02 15:33:33 -0600

    Add concert cmponent

--- src/pages/concerts.js
+++ src/pages/concerts.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import HomeHero from '../components/Hero/HomeHero';
+import Concert from '../components/Concerts/Concert';
 
-const Concerts = () => {
+const concerts = () => {
     return (
         <Layout>
             <HomeHero />
+            <div className='container'>
+                <div className='row'>
+                    <div className='col'>
+                        <Concert />
+                    </div>
+                    <div className='col'></div>
+                    <div className='col'></div>
+                </div>
+            </div>
         </Layout>
     );
 }
 
-export default Concerts;
+export default concerts;
