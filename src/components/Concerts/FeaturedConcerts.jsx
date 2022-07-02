ecd194a89cb509f499e01fdbee0e170727abac51
Francisco Montes <paco@sharpydev.com>
2022-07-02 16:44:46 -0600

    get featured concerts cron containtful

--- src/components/Concerts/FeaturedConcerts.jsx
+++ src/components/Concerts/FeaturedConcerts.jsx
@@ -1,16 +1,53 @@
 import React from "react"
-import Concert from './Concert';
+import Concert from "./Concert"
+import { graphql, useStaticQuery } from "gatsby"
 
 const FeaturedConcerts = () => {
+  const data = useStaticQuery(graphql`
+  {
+    allContentfulFeaturedConcerts {
+      nodes {
+        month
+        name
+        day
+        slug
+        image {
+          file {
+            url
+          }
+        }
+        description {
+          description
+        }
+      }
+    }
+  }
+  `)
+  
+  const concerts = data.allContentfulFeaturedConcerts.nodes
+  console.log(concerts)
+
+  const featuredConcerts = concerts.map( (concert) => {
+    const deatails = {
+        name: concert.name,
+        description: concert.description.description,
+        month: concert.month,
+        day: concert.day,
+        slug: concert.slug,
+        image: concert.image.file.url
+    }
+    return <div className="col-md-4" key={concert.slug}>
+        <Concert concertDetails={deatails} />
+    </div>
+  })
+
   return (
     <div className="featured-concerts">
       <div className="container">
+        <h4>Featured Concerts</h4>
+        <br />
         <div className="row">
-          <div className="col">
-            <Concert />
-          </div>
-          <div className="col"></div>
-          <div className="col"></div>
+            {featuredConcerts}
         </div>
       </div>
     </div>
