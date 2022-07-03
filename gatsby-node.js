84c5d41e28a1ae914db4dc6565f446329c18adcc
Francisco Montes <paco@sharpydev.com>
2022-07-03 11:21:09 -0600

    Add node export dor concert

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,31 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+const path = require("path");
+const { node } = require( "prop-types" );
+
+exports.createPages = async ({ graphql, actions }) => {
+  const concertDetailsTemplate = path.resolve(
+    `./src/templates/ConcertDetails.jsx`
+  )
+
+  //query slugs from Contentful
+  const result = await graphql(`
+    query {
+      allContentfulFeaturedoncerts {
+        nodes {
+          slug
+        }
+      }
+    }
+  `)
+
+  // create pages for each concert
+  const nodes = result.data.allContentfulFeaturedconcerts.nodes
+  nodes.map((node) => {
+    node.url = `/concerts/${node.slug}`
+
+    actions.createPage({
+      path: node.url,
+      component: concertDetailsTemplate,
+      context: { slug: node.slug}
+    })
   })
 }
