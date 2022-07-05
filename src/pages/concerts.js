3a8cebfc641e1fb66bcf54bd58ca1ef9599bfa3a
Francisco Montes <paco@sharpydev.com>
2022-07-04 20:56:48 -0600

    Add helmet

--- src/pages/concerts.js
+++ src/pages/concerts.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import HomeHero from '../components/Hero/HomeHero';
-import FeaturedConcerts from '../components/Concerts/FeaturedConcerts';
+import React from "react"
+import Layout from "../components/Layout"
+import HomeHero from "../components/Hero/HomeHero"
+import FeaturedConcerts from "../components/Concerts/FeaturedConcerts"
+import { Helmet } from "react-helmet"
+
+import concert from '../images/hero/yvette-de-wit-8XLapfNMW04-unsplash.jpg'
 
 const concerts = () => {
-    return (
-        <Layout>
-            <HomeHero />
-            <FeaturedConcerts />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Helmet title="Featured Concerts | ConcertParty">
+        <meta
+          name="description"
+          content="Explore and experience the gratest music on earth."
+        />
+        <meta name="image" content={concert} />
+      </Helmet>
+      <HomeHero />
+      <FeaturedConcerts />
+    </Layout>
+  )
 }
 
-export default concerts;
+export default concerts
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,24 @@
 import React from "react"
+import { Helmet } from "react-helmet"
+
 import Layout from "../components/Layout"
 import HomeHero from "../components/Hero/HomeHero"
 import EmailListFormDisplay from "../components/EmailListForm/EmailListFormDisplay"
 import About from "../components/About/About"
 
+import company from '../images/company/duy-pham-Cecb0_8Hx-o-unsplash.jpg'
+
 const Index = () => {
   return (
     <div>
       <Layout>
+        <Helmet title="Welcome to ConcertParty">
+          <meta
+            name="description"
+            content="Concert Party is a global community connectiong music and show lovers to the concert around the world"
+          />
+          <meta name="image" content={company} />
+        </Helmet>
         <HomeHero />
         <EmailListFormDisplay />
         <About />
