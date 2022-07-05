04f304f30d4c0f4826f38bf0ec4aff97982e6ac0
Francisco Montes <paco@sharpydev.com>
2022-07-04 18:47:19 -0600

    Concert loading text

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,57 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import Tab from "react-bootstrap/Tab"
 import Tabs from "react-bootstrap/Tabs"
-import Concerts from "./Concerts";
+import Concerts from "./Concerts"
 
 import "./dashboard.scss"
 
 const Dashboard = () => {
+  const [concerts, setConcerts] = useState([])
+  const [requesting, setRequesting] = useState(true)
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "https://api.predicthq.com/v1/events/?category=concerts&country=us&limit=10",
+      headers: {
+        Authorization: `Bearer ${process.env.GATSBY_EVENTS_API}`,
+      },
+    })
+      .then(res => {
+        setConcerts(res.data.results)
+        setRequesting(false)
+      })
+      .catch(err => {
+        console.log(err)
+        setRequesting(false)
+      })
+  }, [])
+
+  console.log("Concerts: ", concerts)
+
+let allConcerts
+
+if(requesting=== false && concerts.length >1){
+  concerts.map(concert => {
+      const venue = concert.entities.length !==0 ? concert.entities[0].name: "Venue N/A"
+      const concertInfo = {
+        title: concert.title,
+        startDate: concert.start,
+        endDate: concert.end,
+        tags: concert.labels,
+        venue: venue
+      }
+      return <Concerts concertInfo={concertInfo} key={concert.id}/>
+    })
+
+}else if (requesting=== false && concerts.length === 0){
+  allConcerts = <p>Nothing to see here</p>
+}else{
+  allConcerts= <h5 className="text-center">Loading...</h5>
+}
+
+
+
   return (
     <div className="dashboard shadow mb-5 bg-white rounded p-3">
       <div className="container">
@@ -15,8 +61,7 @@ const Dashboard = () => {
         <Tabs defaultActiveKey="upcoming">
           <Tab eventKey="upcoming" title="Upcoming">
             <div className="upcoming">
-                <Concerts />
-                <Concerts />
+              {allConcerts}
             </div>
           </Tab>
           <Tab eventKey="booked" title="Booked">
